899ce2ff4a9459584afc45afd980c6f3d986da7a
lichangao <changao.li.life@outlook.com>
2022-11-16 00:43:38 +0800

    洛谷 P1714

--- basic_data_structure/monotone_queue/README.md
+++ basic_data_structure/monotone_queue/README.md
@@ -28,7 +28,7 @@ for (int i = 0; i < n; i++) {
 | 滑动窗口最值 | [洛谷 P1886](./洛谷%20P1886.cpp) | [查看原题](https://www.luogu.com.cn/problem/P1886) |
 | 滑动窗口最值 | [洛谷 P1440](./洛谷%20P1440.cpp) | [查看原题](https://www.luogu.com.cn/problem/P1440) |
 | 滑动窗口最值 | [洛谷 P2032](./洛谷%20P2032.cpp) | [查看原题](https://www.luogu.com.cn/problem/P2032) |
-| 带长度限制的最大子数组和 | 洛谷 P1714 | [查看原题](https://www.luogu.com.cn/problem/P1714) |
+| 带长度限制的最大子数组和 | [洛谷 P1714](./洛谷%20P1714.cpp) | [查看原题](https://www.luogu.com.cn/problem/P1714) |
 | 带长度限制的最大子数组和 | 洛谷 P2629 | [查看原题](https://www.luogu.com.cn/problem/P2629) |
 | 单调队列优化 DP | 洛谷 P3957 | [查看原题](https://www.luogu.com.cn/problem/P3957) |
 | 单调队列优化 DP | 洛谷 P1725 | [查看原题](https://www.luogu.com.cn/problem/P1725) |
---
+++ basic_data_structure/monotone_queue/洛谷 P1714.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <deque>
+#include <climits>
+
+using namespace std;
+
+const int N = 500010;
+int x, sum[N];
+
+deque<int> q;
+
+int main() {
+    int n, m;
+    scanf("%d%d", &n, &m);
+    
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &x);
+        sum[i] = sum[i - 1] + x;
+    }
+    
+    q.push_back(0);
+    
+    int ans = INT_MIN;
+    for (int i = 1; i <= n; i++) {
+        while (!q.empty() && i - m > q.front()) {
+            q.pop_front();
+        }
+        while (!q.empty() && sum[q.back()] > sum[i]) {
+            q.pop_back();
+        }
+        q.push_back(i);
+        ans = max(ans, sum[i] - sum[q.front()]);
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
